ea0d9a8b7522ce4d6ac2e4ba0030b709f0b798ec
Austen <aortmeier20@gmail.com>
2024-05-21 17:12:35 -0500

    Progress!!!

--- employeeSQL.sql
+++ employeeSQL.sql
@@ -7,53 +7,65 @@ DROP TABLE salaries;
 
 
 
-CREATE TABLE titles (
-	title_id VARCHAR NOT NULL,
-	title VARCHAR NOT NULL,
-	CONSTRAINT pk_titles		PRIMARY KEY (
-		title_id))
+CREATE TABLE "titles" (
+	"title_id" VARCHAR(10) NOT NULL,
+	"title" VARCHAR(30) NOT NULL,
+	CONSTRAINT "pk_titles"		PRIMARY KEY (
+		"title_id"))
 ;
-
-CREATE TABLE departments (
-	dept_no VARCHAR NOT NULL,
-	dept_name VARCHAR NOT NULL,
-	CONSTRAINT pk_departments		PRIMARY KEY (
-		dept_no))
-;
-
-CREATE TABLE dept_emp (
-	emp_no INTEGER NOT NULL,
-	dept_no VARCHAR NOT NULL)
-;
-
-CREATE TABLE dept_manager (
-	dept_no VARCHAR NOT NULL,
-	emp_no INTEGER NOT NULL
-)
-;
-CREATE TABLE employees (
-	emp_no INTEGER NOT NULL,
-	emp_title_id VARCHAR NOT NULL,
-	birth_date VARCHAR NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	sex VARCHAR NOT NULL,		
-	hire_date VARCHAR NOT NULL,
-	CONSTRAINT pk_employees		PRIMARY KEY (
-		emp_no))
-;
-CREATE TABLE salaries (
-	emp_no INTEGER,
-	salary INTEGER
-)
-;
-
-
-
 
 SELECT * FROM titles;
+
+
+CREATE TABLE "departments" (
+	"dept_no" VARCHAR(10) NOT NULL,
+	"dept_name" VARCHAR(30) NOT NULL,
+	CONSTRAINT "pk_departments"		PRIMARY KEY (
+		"dept_no"))
+;
+
 SELECT * FROM departments;
+
+CREATE TABLE "dept_emp" (
+	"emp_no" INTEGER NOT NULL,
+	"dept_no" VARCHAR(10) NOT NULL)
+;
+
 SELECT * FROM dept_emp;
+
+
+CREATE TABLE "dept_manager" (
+	"dept_no" VARCHAR(10) NOT NULL,
+	"emp_no" INTEGER NOT NULL
+)
+;
+
+
 SELECT * FROM dept_manager;
+
+
+CREATE TABLE "employees" (
+	"emp_no" INTEGER NOT NULL,
+	"emp_title_id" VARCHAR(10) NOT NULL,
+	"birth_date" VARCHAR NOT NULL,
+	"first_name" VARCHAR(20) NOT NULL,
+	"last_name" VARCHAR(20) NOT NULL,
+	"sex" VARCHAR(1) NOT NULL,		
+	"hire_date" VARCHAR NOT NULL,
+	CONSTRAINT "pk_employees"		PRIMARY KEY (
+		"emp_no"))
+;
+
 SELECT * FROM employees;
+
+CREATE TABLE "salaries" (
+	"emp_no" INTEGER,
+	"salary" INTEGER
+)
+;
+
 SELECT * FROM salaries;
+
+
+
+
