6a98ac8c37d8b3ba771c931a5bf3b651c82f2f9a
Austen <aortmeier20@gmail.com>
2024-05-21 11:44:37 -0500

    trying but have no idea

--- employeeSQL.sql
+++ employeeSQL.sql
@@ -1,11 +1,52 @@
-DROP TABLE IF EXISTS titles
+DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS employees;
 
 
-CREATE TABLE "titles" (
-	"title_id" VARCHAR(10) NOT NULL,
-	"title" VARCHAR(30) NOT NULL,
-	CONSTRAINT "pk_titles" PRIMARY KEY (
-		"title_id"))
+CREATE TABLE titles (
+	title_id VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
+	CONSTRAINT pk_titles		PRIMARY KEY (
+		title_id))
+;
 
-SELECT * FROM titles
+CREATE TABLE departments (
+	dept_no VARCHAR NOT NULL,
+	dept_name VARCHAR NOT NULL,
+	CONSTRAINT pk_departments		PRIMARY KEY (
+		dept_no))
+;
+
+CREATE TABLE dept_emp (
+	emp_no INTEGER NOT NULL,
+	dept_no VARCHAR NOT NULL)
+;
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR NOT NULL,
+	emp_no INTEGER NOT NULL
+)
+;
+CREATE TABLE employees (
+	emp_no INTEGER NOT NULL,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date VARCHAR NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,		
+	hire_date VARCHAR NOT NULL,
+	CONSTRAINT pk_employees		PRIMARY KEY (
+		emp_no))
+;
+CREATE TABLE salaries (
+	emp_no INTEGER NOT NULL,
+	salary INTEGER NOT NULL
+)
+;
+
+
+SELECT * FROM titles;
 
